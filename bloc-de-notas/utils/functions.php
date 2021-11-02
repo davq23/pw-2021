8810ba4a9a01f82c542572fb11bf7e93490dd24c
= <=>
2021-11-02 13:22:58 -0400

    [functions.php] align functions on same line

--- bloc-de-notas/utils/functions.php
+++ bloc-de-notas/utils/functions.php
@@ -1,16 +1,15 @@
 <?php
 
-function filterSanitizePath(string $path) : string
-{
+function filterSanitizePath(string $path): string {
     $pathSanitized = str_replace('../', '', $path);
     return str_replace('./', '', $pathSanitized);
 }
 
 // Does not support flag GLOB_BRACE
 function rglob($pattern, $flags = 0) {
-    $files = glob($pattern, $flags); 
-    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+    $files = glob($pattern, $flags);
+    foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
+        $files = array_merge($files, rglob($dir . '/' . basename($pattern), $flags));
     }
     return $files;
 }
