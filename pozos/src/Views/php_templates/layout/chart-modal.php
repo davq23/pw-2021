124af43f2f33c6c7ad8d1eea010768fc8beb3cf1
= <=>
2021-11-06 18:47:06 -0400

    [chart-modal.php] add form to search measurements

--- pozos/src/Views/php_templates/layout/chart-modal.php
+++ pozos/src/Views/php_templates/layout/chart-modal.php
@@ -1,8 +1,8 @@
 <!-- Modal -->
 <div class="modal fade" id="modalChart" data-bs-backdrop="static" data-bs-keyboard="false"
-     url="<?= BASE_URL . 'well/measurements' ?>"
+     url="<?= BASE_URL . 'well/measurements' ?>" callback="updateData"
      tabindex="-1" aria-labelledby="modalChartLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog min-vw-75">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalChartLabel">
@@ -10,8 +10,50 @@
                 </h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body" >
-                <canvas id="chart-panel"></canvas>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <a class="btn btn-primary" data-bs-toggle="collapse" href="#filterCollapse" role="button" aria-expanded="false" aria-controls="filterCollapse">
+                                Filter options
+                            </a>
+                        </div>
+                        <form class="ajax-form collapse row submit-on-input mt-3"
+                              id="filterCollapse"
+                              method="get"
+                              action="<?= BASE_URL . 'well/measurements' ?>">
+                            <input type="hidden" name="oil_well" />
+                            <div class="col">
+                                <label for="year" class="form-label">Year</label>
+                                <input class="form-control" type="number"
+                                       id="year"
+                                       name="year"
+                                       min="2000" max="<?= date('Y') ?>"/>
+                            </div>
+                            <div class="col">
+                                <label for="month" class="form-label">Month</label>
+                                <input class="form-control" type="number"
+                                       id="month"
+                                       name="month"
+                                       min="1" max="12"/>
+                            </div>
+                            <div class="col">
+                                <label for="day" class="form-label">Day</label>
+                                <input class="form-control" type="number"
+                                       id="day"
+                                       name="day"
+                                       min="0" max="31"/>
+                            </div>
+                        </form>
+
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <canvas id="chart-panel"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
