3d6bc9fcf21fe76395bc3ff4bf3fd6fe3c4f3cf7
= <=>
2021-11-10 14:07:39 -0400

    [MySQLiUserRepository.php] reformat functions, add user role

--- laboratorio/src/Repositories/MySQLi/MySQLiUserRepository.php
+++ laboratorio/src/Repositories/MySQLi/MySQLiUserRepository.php
@@ -8,23 +8,24 @@ use Repositories\UserRepository;
 
 class MySQLiUserRepository extends MySQLiRepository implements UserRepository
 {
+
     /** {@inheritDoc} */
-    public function findById($id): User
-    {
+    public function findById($id): User {
         $user = null;
         $username = null;
         $email = null;
         $password = null;
+        $userRole = null;
 
-        $statement = $this->mysqli()->prepare('SELECT username, email, password FROM users WHERE id = ? LIMIT 1');
+        $statement = $this->mysqli()->prepare('SELECT username, email, password, user_role FROM users WHERE id = ? LIMIT 1');
         $statement->bind_param('s', $id);
 
-        $statement->bind_result($username, $email, $password);
+        $statement->bind_result($username, $email, $password, $userRole);
 
         $statement->execute();
 
         while ($statement->fetch()) {
-            $user = new User($id, $email, $username, $password);
+            $user = new User($id, $email, $username, $password, $userRole);
         }
 
         if (is_null($user)) {
@@ -35,22 +36,22 @@ class MySQLiUserRepository extends MySQLiRepository implements UserRepository
     }
 
     /** {@inheritDoc} */
-    public function findByEmail(string $email): User
-    {
+    public function findByEmail(string $email): User {
         $user = null;
         $id = null;
         $username = null;
         $password = null;
+        $userRole = null;
 
         $statement = $this->mysqli()->prepare('SELECT * FROM users WHERE email = ? LIMIT 1');
         $statement->bind_param('s', $email);
 
-        $statement->bind_result($id, $username, $email, $password);
+        $statement->bind_result($id, $username, $email, $password, $userRole);
 
         $statement->execute();
 
         while ($statement->fetch()) {
-            $user = new User($id, $email, $username, $password, true);
+            $user = new User($id, $email, $username, $password, $userRole, true);
         }
 
         if (is_null($user)) {
@@ -61,22 +62,22 @@ class MySQLiUserRepository extends MySQLiRepository implements UserRepository
     }
 
     /** {@inheritDoc} */
-    public function findByUsername(string $username): User
-    {
+    public function findByUsername(string $username): User {
         $user = null;
         $id = null;
         $email = null;
         $password = null;
+        $userRole = null;
 
         $statement = $this->mysqli()->prepare('SELECT * FROM users WHERE username = ? LIMIT 1');
         $statement->bind_param('s', $username);
 
-        $statement->bind_result($id, $username, $email, $password);
+        $statement->bind_result($id, $username, $email, $password, $userRole);
 
         $statement->execute();
 
         while ($statement->fetch()) {
-            $user = new User($id, $email, $username, $password);
+            $user = new User($id, $email, $username, $password, $userRole);
         }
 
         if (is_null($user)) {
@@ -87,22 +88,25 @@ class MySQLiUserRepository extends MySQLiRepository implements UserRepository
     }
 
     /** {@inheritDoc} */
-    public function registerUser(User $user): User
-    {
+    public function registerUser(User $user): User {
         $statement = $this->mysqli()->prepare(
-            'INSERT INTO users (email, username, password) VALUES (?, ?, ?)'
+            'INSERT INTO users (email, username, password, user_role) VALUES (?, ?, ?, ?)'
         );
 
         $username = $user->getUsername();
         $email = $user->getEmail();
         $password = $user->getPassword();
+        $userRole = $user->getUserRole();
 
-        $statement->bind_param('sss', $email, $username, $password);
+        $statement->bind_param('ssss', $email, $username, $password, $userRole);
 
-        $statement->execute();
+        if (!$statement->execute()) {
+            throw new Exception($this->mysqli()->error);
+        }
 
         $user->setId($this->mysqli()->insert_id);
 
         return $user;
     }
+
 }
