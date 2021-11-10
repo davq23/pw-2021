96345e8b5a3ed462ece050ff5b09e556aa640ed3
= <=>
2021-11-10 13:57:21 -0400

    [autoload.php] minor formatting changes

--- laboratorio/src/autoload.php
+++ laboratorio/src/autoload.php
@@ -1,13 +1,12 @@
 <?php
+
 const BASE_URL = 'http://localhost/pw-2021/laboratorio/public/';
 
-spl_autoload_register(function ($className)
-{
- $class = str_replace("\\", DIRECTORY_SEPARATOR, $className);
+spl_autoload_register(function ($className) {
+    $class = str_replace("\\", DIRECTORY_SEPARATOR, $className);
     $path = __DIR__ . '/' . $class . '.php';
 
-    if (file_exists($path))
-    {
+    if (file_exists($path)) {
         require_once $path;
     }
 });
