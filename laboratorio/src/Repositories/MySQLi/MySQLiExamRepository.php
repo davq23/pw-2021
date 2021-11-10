400114349fdacfd06da3d3e2441f12c9f37e1f1a
= <=>
2021-11-10 14:06:26 -0400

    [MySQLiExamRepository.php] reformat functions, add findAllByUserId method

--- laboratorio/src/Repositories/MySQLi/MySQLiExamRepository.php
+++ laboratorio/src/Repositories/MySQLi/MySQLiExamRepository.php
@@ -9,8 +9,7 @@ class MySQLiExamRepository extends MySQLiRepository implements ExamRepository
 {
 
     /** {@inheritDoc} */
-    public function fetchAll(int $limit, int $offset): array
-    {
+    public function fetchAll(int $limit, int $offset): array {
         $exams = array();
         $examArray = $this->dbConnection->fetchAllOffsetPaginated('exams', '*', $limit, $offset);
 
@@ -22,14 +21,12 @@ class MySQLiExamRepository extends MySQLiRepository implements ExamRepository
     }
 
     /** {@inheritDoc} */
-    public function registerExam(Exam $exam): Exam
-    {
+    public function registerExam(Exam $exam): Exam {
         return new Exam();
     }
 
     /** {@inheritDoc} */
-    public function getExamCountByUserId($userId): int
-    {
+    public function getExamCountByUserId($userId): int {
         $statement = $this->mysqli()->prepare('SELECT COUNT(id) AS num_exams FROM exams WHERE user_id = ?');
 
         $statement->bind_param('s', $userId);
@@ -45,4 +42,26 @@ class MySQLiExamRepository extends MySQLiRepository implements ExamRepository
 
         return $numExams;
     }
+
+    /** {@inheritDoc} */
+    public function fetchAllByUserId(int $limit, int $offset, $userId): array {
+        $exams = array();
+
+        $statement = $this->mysqli()->prepare('SELECT * FROM exams WHERE user_id = ? LIMIT ? OFFSET ?');
+
+        $statement->bind_param('sii', $userId, $limit, $offset);
+
+        $statement->execute();
+
+        $result = $statement->get_result();
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $exams[] = new Exam();
+        }
+
+        $statement->close();
+
+        return $exams;
+    }
+
 }
